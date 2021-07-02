411fa4e09c5144be7927810c119f9873192119ca
Melodiean <melodynsita@gmail.com>
2021-07-02 12:59:00 +0300

    Edit functionality

---
+++ src/context/context.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, createContext } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = (props) => {
+  const [user, setUser] = useState({
+    isAuth: "",
+    UID: "",
+    email: "",
+    username: "",
+    role: "",
+  });
+  const [isAuth, setIsAuth] = useState(false);
+  const [parcel, setParcel] = useState("");
+
+  const authUser = async () => {
+    const res = await fetch("http://mnscapi.herokuapp.com:3020/api/v1/auth/profile", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((d) => {
+        if (d.isAuth) {
+          setIsAuth(true);
+          setUser(d);
+          console.log(d)
+        }
+        setIsAuth(false);
+      })
+      .catch((er) => {
+        console.log(er.message);
+        setIsAuth(false)
+      });
+
+    return res;
+  };
+
+  useEffect(() => {
+    authUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={[user,setUser,
+      isAuth, setIsAuth,parcel,setParcel]}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
